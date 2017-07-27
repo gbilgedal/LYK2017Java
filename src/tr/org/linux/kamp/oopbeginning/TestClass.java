ba1bca4b9990a891dc0b9673f52b6dd68a7d7c84
Bilge <lagala999@gmail.com>
2017-07-27 16:46:12 +0300

    tested our classes

--- src/tr/org/linux/kamp/oopbeginning/TestClass.java
+++ src/tr/org/linux/kamp/oopbeginning/TestClass.java
@@ -2,7 +2,14 @@ package tr.org.linux.kamp.oopbeginning;
 
 public class TestClass {
 	public static void main(String[] args) {
-		
+		Cat lily = new Cat("Black","Black", 3, "Scottish Shorthair");
+		lily.meow();
+		lily.scratch();
+		Snake hale = new Snake("red","black",12,"normal snake");
+		hale.hasNoLegs();
+		MockingBird fuzzy = new MockingBird("black","white",2,"Lovely Mocking Bird");
+		fuzzy.tweet(); 
+		fuzzy.fly();
 	}
 
 }
