4a604194f64ce7dad81cbe8b4972f24b81210bb4
Bilge <lagala999@gmail.com>
2017-07-27 20:53:42 +0300

    vehicle example, oop

--- src/tr/org/linux/kamp/oopbeginning/TestClass.java
+++ src/tr/org/linux/kamp/oopbeginning/TestClass.java
@@ -2,14 +2,19 @@ package tr.org.linux.kamp.oopbeginning;
 
 public class TestClass {
 	public static void main(String[] args) {
-		Cat lily = new Cat("Black","Black", 3, "Scottish Shorthair");
+		Cat lily = new Cat("Black", "Black", 3, "Scottish Shorthair");
 		lily.meow();
 		lily.scratch();
-		Snake hale = new Snake("red","black",12,"normal snake");
+		Snake hale = new Snake("red", "black", 12, "normal snake");
 		hale.hasNoLegs();
-		MockingBird fuzzy = new MockingBird("black","white",2,"Lovely Mocking Bird");
-		fuzzy.tweet(); 
+		MockingBird fuzzy = new MockingBird("black", "white", 2, "Lovely Mocking Bird");
+		fuzzy.tweet();
 		fuzzy.fly();
+
+		Object o = lily;
+		Animal a = lily;
+		Carnivore c = lily;
+		Mammal m = lily;
 	}
 
 }
